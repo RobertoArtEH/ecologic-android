984102df1136f20e227b9acb04be35be330fc121
Roberto Esqueda <roberto.raeh@gmail.com>
2019-11-23 21:08:17 -0600

    Login details fixed

--- app/src/main/java/com/example/eco_logic/LoginActivity.java
+++ app/src/main/java/com/example/eco_logic/LoginActivity.java
@@ -11,4 +11,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    @Override
+    public void onBackPressed() { }
 }
