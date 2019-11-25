519605c311852b9c7f2dc2a38dcbb8de6501d4c4
Roberto Esqueda <roberto.raeh@gmail.com>
2019-11-25 08:32:20 -0600

    Bottom navigation added

---
+++ app/src/main/java/com/example/eco_logic/BottomNavigationActivity.java
@@ -0,0 +1,55 @@
+package com.example.eco_logic;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class BottomNavigationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_navigation);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
+        Fragment initialFragment = new PlantsFragment();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, initialFragment).commit();
+        }
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    switch (menuItem.getItemId()) {
+                        case R.id.nav_plants:
+                            selectedFragment = new PlantsFragment();
+                            break;
+                        case R.id.nav_weather:
+                            selectedFragment = new WeatherFragment();
+                            break;
+                        case R.id.nav_account:
+                            selectedFragment = new AccountFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                    return true;
+                }
+            };
+}
--- app/src/main/java/com/example/eco_logic/LoginActivity.java
+++ app/src/main/java/com/example/eco_logic/LoginActivity.java
@@ -1,15 +1,32 @@
 package com.example.eco_logic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btnLogin = findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        // Intent temporal para probar el bottom navigation
+        Intent intent = new Intent(LoginActivity.this, BottomNavigationActivity.class);
+        startActivity(intent);
     }
 
     @Override
