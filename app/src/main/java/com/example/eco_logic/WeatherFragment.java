b11ec98b86b7ad344d3f2f6a23ded554aed71fd4
Roberto Esqueda <roberto.raeh@gmail.com>
2019-11-27 07:32:27 -0600

    Weather fragment

--- app/src/main/java/com/example/eco_logic/WeatherFragment.java
+++ app/src/main/java/com/example/eco_logic/WeatherFragment.java
@@ -1,13 +1,30 @@
 package com.example.eco_logic;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.concurrent.Executor;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class WeatherFragment extends Fragment {
 
@@ -15,5 +32,40 @@ public class WeatherFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_weather, container, false);
+    }
+
+    private FusedLocationProviderClient client;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Toast.makeText(getActivity(), "Test!", Toast.LENGTH_LONG).show();
+
+        requestPermission();
+
+        client = LocationServices.getFusedLocationProviderClient(getActivity());
+
+        if ( ContextCompat.checkSelfPermission( getActivity(), ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+            return;
+        }
+
+        client.getLastLocation().addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if(location != null) {
+                    Double lat = location.getLatitude();
+                    Double lon = location.getLongitude();
+
+                    String loc = lat + ", " + lon;
+
+                    Toast.makeText(getActivity(), loc, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(getActivity(), new String[]{ACCESS_FINE_LOCATION}, 1);
     }
 }
